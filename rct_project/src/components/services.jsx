27c7126490e9d09782b1cd5cc70e95924e8fd56c
BhavikDholu <bhavikdholu1998@gmail.com>
2022-11-10 22:43:07 +0530

    day-3

--- rct_project/src/components/services.jsx
+++ rct_project/src/components/services.jsx
@@ -1,19 +1,19 @@
 import {Box,Image,Checkbox,Text,Center} from "@chakra-ui/react";
 
-export default function Services(){
-    
+export default function Services(props){
+    const {title,logo,color} = props;
 
     return (
-        <Box border="1px solid gray" borderRadius={10} p="10px" width="9%">
+        <Box border="1px solid gray" borderRadius={10} p="10px" width="10%">
             <Box align="left">
-            <Checkbox size='lg' colorScheme='orange' Checked></Checkbox>
+            <Checkbox size='lg' colorScheme={color} Checked></Checkbox>
             </Box>
             <Box>
                 <Center>
-                <Image src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/design_icon.png"
+                <Image src={logo}
                 alt="icon" width="50px"/>
                 </Center>
-                <Text>Creative & Design</Text>
+                <Text>{title}</Text>
             </Box>
         </Box>
     )
--- rct_project/src/components/topSec.jsx
+++ rct_project/src/components/topSec.jsx
@@ -1,7 +1,82 @@
 import { Box, Image ,Text , Button, Flex } from "@chakra-ui/react";
 import {ArrowForwardIcon} from "@chakra-ui/icons";
 import Services from "./services";
-
+const logo=[
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/hulu.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/bd.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/canva.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/nhl.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/coca_cola.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/hubspot.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/Electronic_Arts.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/nautica.png"
+    },
+    {
+        url : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/customers/logos-v2/universal.png"
+    }
+]
+const serviceData=[
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/design_icon.png",
+        title : "Creative & Design",
+        color : "pink"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/dev_icon.png",
+        title : "Software development",
+        color : "green"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/marketing_icon.png",
+        title : "Marketing",
+        color : "pink"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/pmo_icon.png",
+        title : "Project management",
+        color : "yellow"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/crm_icon.png",
+        title : "Sales & CRM",
+        color : "blue"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/task_icon.png",
+        title : "Task management",
+        color : "blue"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/hr_icon.png",
+        title : "HR",
+        color : "red"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/operations_icon.png",
+        title : "Operations",
+        color : "blue"
+    },
+    {
+        logo : "https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/icons/workflows_icon.png",
+        title : "More Workflows",
+        color : "blue"
+    }
+]
 export default function TopSec(){
     return(
         <Box bg="linear-gradient(rgb(0, 1, 51) 0%, rgb(15, 16, 72) 100%)" color='white'pt={50}>
@@ -13,15 +88,9 @@ export default function TopSec(){
             </Box>
             <Box width="90%" margin="auto" mt="30px">
                 <Flex  gap="12px" justifyContent="center">
-                    <Services/>
-                    <Services/>
-                    <Services/>
-                    <Services/>
-                    <Services/>
-                    <Services/>
-                    <Services/>
-                    <Services/>
-                    <Services/>
+                    {
+                        serviceData.map((e)=><Services title={e.title} logo={e.logo} color={e.color} key={e.title}/>)
+                    }
                 </Flex>
             </Box>
             <Box pt={10}>
@@ -30,6 +99,18 @@ export default function TopSec(){
             </Box>
             <Box>
                 <Image src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured%20images/Home%20Page%20-%202022%20Rebrand/first_fold/image.png" alt="image"/>
+            </Box>
+            <Box>
+                <Text>Trusted by 152,000+ customers worldwide</Text>
+                <Box>
+                    <Flex justifyContent="center">
+                        {
+                            logo.map((e)=><Box>
+                                <Image src={e.url} alt="logo"/>
+                            </Box>)
+                        }
+                    </Flex>
+                </Box>
             </Box>
         </Box>
     )
